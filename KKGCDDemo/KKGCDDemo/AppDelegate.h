0444997496da15dcbb1ade4bb63a24ea94107dda
Daniel <yebin@gtjas.com>
2019-01-13 10:08:26 +0800

    Demo初始化提交

---
+++ KKGCDDemo/KKGCDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKGCDDemo
+//
+//  Created by keke on 2019/1/13.
+//  Copyright © 2019 kekeyezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKGCDDemo/KKGCDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKGCDDemo
+//
+//  Created by keke on 2019/1/13.
+//  Copyright © 2019 kekeyezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
